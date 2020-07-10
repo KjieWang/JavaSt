498bf1b0033af51d9f82dcae88617722f6499c54
KjieWang <2672126643@qq.com>
2020-07-10 13:54:02 +0800

    第二次修改测试

--- JavaPrc/src/hello/HelloWord.java
+++ JavaPrc/src/hello/HelloWord.java
@@ -4,5 +4,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("helloWorld!");
         System.out.println("第一次修改测试");
+        System.out.println("第二次修改");
     }
 }
